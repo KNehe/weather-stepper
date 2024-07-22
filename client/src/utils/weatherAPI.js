469cf1854f76030f8d72d5acadb353e9f519d745
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2024-07-22 20:49:51 +0300

    dockerize the project - add requirements.txt for django project - configure allowed hosts and cors allowed origins in settings.py - add dockerfiles and docker compose files

--- client/src/utils/weatherAPI.js
+++ client/src/utils/weatherAPI.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
 
 export const fetchWeatherData = async (lat, long) => {
   const response = await fetch(`${API_BASE_URL}/weather/?lat=${lat}&long=${long}`);
